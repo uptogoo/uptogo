7c951ecad4e84a0d0d36371973005b07bb67a388
Up To Go <95484477+uptogoo@users.noreply.github.com>
2022-03-04 01:15:28 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-/* Image Gallery */
-/* Switching between the full image and the rest of the images  */
-
-function myFunction(smallImg)
-{
-    var fullImg = document.getElementById("Full");
-    fullImg.src = smallImg.src;
-}
