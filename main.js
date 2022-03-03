68ae2bf9546607e09b8c15e81956d5f6844e933b
Up To Go <95484477+uptogoo@users.noreply.github.com>
2022-03-03 16:44:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Image Gallery */
+/* Switching between the full image and the rest of the images  */
+
+function myFunction(smallImg)
+{
+    var fullImg = document.getElementById("Full");
+    fullImg.src = smallImg.src;
+}
